6250feccfa526ad9bf507f1201630d22fd8e2ffc
Vikas96610 <vikasrekha5586@gmail.com>
2022-11-02 15:13:14 +0530

    different types of loops

---
+++ loop exp.py
@@ -0,0 +1,27 @@
+x = ["good","bad","very good"]
+for i in x:
+    print(i)
+    
+y = ["vikas"]
+for i in y:
+    print(i)
+    
+    
+for letter in "abcdefgh":
+    if letter == 'i':
+        break
+    print(letter)
+    
+    
+for i in range (12):
+    print(i)
+    
+for i in range(1,4):
+    print(i)
+else:
+    print("no break")
+    
+    
+a = [1,2,3,4]
+(a.append('95'))
+print(a)
